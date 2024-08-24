d34638108bce669ac7d2cd17a927731df67431a7
Maksim Palchevski <83406787+YraBot@users.noreply.github.com>
2024-08-24 13:52:58 +0300

    added bullets

--- platformer_sln/platformer/Hero.h
+++ platformer_sln/platformer/Hero.h
@@ -9,9 +9,17 @@ class Hero {
 private:
 	shared_ptr<RenderWindow> window;
 	ComplexFields cf;
+	//cellka globalposmapa = cellka(0, 0);
 	bool isGround = true;
 	bool _direction = true;//false(0) - left, true(1) - right
+	//Vector2f size = Vector2f(Pr::sz / 2 - 20, Pr::sz / 2 - 20);
 	RectangleShape shape = InitialRectangleShape(Vector2f(Pr::sz - 40, Pr::sz - 40), Color(0, 0, 0), Vector2f(((Pr::lengthj) / 2 + 0.5) * Pr::sz, ((Pr::lengthi) / 2 + 0.5) * Pr::sz), 1, Color(220, 50, 50));
+	//Vector2f pos = shape.getPosition();
+	//CircleShape pmain = InitialCircleShape(2, Color::Red, pos);
+	//CircleShape pTL = InitialCircleShape(2, Color::Red, Vector2f(pos.x - size.x, pos.y - size.y)), pTR = InitialCircleShape(2, Color::Red, Vector2f(pos.x + size.x, pos.y - size.y));
+	//CircleShape pBL = InitialCircleShape(2, Color::Red, Vector2f(pos.x - size.x, pos.y + size.y)), pBR = InitialCircleShape(2, Color::Red, Vector2f(pos.x + size.x, pos.y + size.y));
+	//Vector2f speed = Vector2f(0, 0);
+	//Vector2f localpos = Vector2f(Pr::sz / 2, Pr::sz - size.y);
 public:
 	Hero(shared_ptr<RenderWindow> window);
 	RectangleShape getShape();
--- platformer_sln/platformer/bullet.h
+++ platformer_sln/platformer/bullet.h
@@ -7,39 +7,46 @@
 
 using namespace std;
 using namespace sf;
+
 class Bullet {
 protected:
-    const float bulletSpeed = 0.0f;
+    const float bulletSpeed = 20.0f;
     const float bulletRadius = 15;
     bool direction;
     CircleShape bul;
-    
-    shared_ptr<RenderWindow> window;
+    static vector<Bullet*> bullets;
 
 public:
+#pragma region Working with static
+    static void MoveBullets() {
+        for (Bullet* b : bullets)
+            b->Move();
+    }
+
+    void DeleteBullet(Bullet* b) {
+        auto it = std::find(bullets.begin(), bullets.end(), b);
+        if (it != bullets.end()) {
+            bullets.erase(it);
+        }
+    }
+#pragma endregion
 
     Bullet(shared_ptr<RenderWindow> window, Color col, Vector2f heroPos, bool direction) {
         this->direction = direction;
         bul = InitialCircleShape(bulletRadius, col, heroPos);  // Assuming InitialCircleShape is defined elsewhere
-        
-        this->window = window;
-
+        bullets.push_back(this);
     }
 
     void Move() {
-        //cout << bul.getPosition().x <<" " << bul.getPosition().y << endl;
         bul.move(Vector2f((direction ? bulletSpeed : -bulletSpeed), 0));
-        //Draw();
     }
 
-    //void Destroy() {
-    //    DeleteBullet(this);
-    //}
-
-    void Draw() {
-        window->draw(bul);
+    void Destroy() {
+        DeleteBullet(this);
+        // Do not use delete this; unless you are absolutely sure it was allocated with new
     }
-    
+
     ~Bullet() {
+        // Destructor will be called when the object is destroyed, so manage your resources here if necessary
     }
 };
